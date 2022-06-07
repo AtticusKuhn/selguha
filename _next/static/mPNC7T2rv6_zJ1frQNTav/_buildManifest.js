7d25c6d7e51d36a39d6c81eaaf62d6973957605e
eulerthedestroyer <eulerthedestroyer@gmail.com>
2022-06-07 13:23:07 -0700

    Initial dist subtree commit

---
+++ _next/static/mPNC7T2rv6_zJ1frQNTav/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/311-78cbb81ce9aea20f.js","static/chunks/pages/index-1ca0cacba9b33dfc.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
